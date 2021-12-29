229a061f643cfd22352eda457d00c5f7ff3818d1
Rillian Grant <rillian.grant@gmail.com>
2021-12-29 14:22:27 +0000

    Done but messy

--- wm.c
+++ wm.c
@@ -1,6 +1,7 @@
 #include <ncurses.h>
 #include <panel.h>
 
+#include "config.h"
 #include "lib/headers/debug.h"
 #include "lib/dll.h"
 
@@ -8,8 +9,9 @@
  * @brief Contains all window data.
  */
 typedef struct {
-    char window_buffer[1000];       /**< Primary source for the text the window contains. Wrap around when you reach the end. Size could be hardcoded, or changed on startup with config files. Allocating swap is too much work */
-    int window_buffer_position;     /**< Window buffer position to next write to. Also where to start reading from */
+    char window_buffer[WINDOW_TEXT_BUFFER_SIZE];       /** Primary source for the text the window contains. Wrap around when you reach the end. Size could be hardcoded, or changed on startup with config files. Allocating swap is too much work */
+    int window_buffer_start_pos;
+    int window_buffer_end_pos;
     int x_pos;
     int y_pos;
     int height;
@@ -57,7 +59,9 @@ int main() {
     while (1) {
         debug_print("Mode %i\n", mode);
         debug_print("Waiting for input...\n");
+
         key = getch();
+        
         debug_print("Key with integer representation %i was pressed. Displayed as %c (n.b. This could be wrong)\n", key, key);
 
         if (mode == no_mode) {
@@ -67,21 +71,40 @@ int main() {
             case 'e':
                 mode = edit_mode;
                 cwm_window_status(current_win, 'e');
+                continue;
                 break;
             case 'm':
                 mode = move_mode;
                 cwm_window_status(current_win, 'm');
+                continue;
                 break;
             case 'r':
                 mode = resize_mode;
                 cwm_window_status(current_win, 'r');
+                continue;
+                break;
+            case 'n':
+                ;
+                PANEL *new_window = cwm_window_create(10, 55, 7, 55);
+                current_win = new_window;
+                cwm_window_refresh(current_win);
+                continue;
+                break;
+            case 'd':
+                ;
+                PANEL *to_delete = current_win;
+                current_win = panel_below(current_win);
+                free(panel_userptr(to_delete));
+                del_panel(to_delete);
+                update_panels();
+                doupdate();
+                continue;
                 break;
             case 9:
-                oc = current_win;
-                current_win = other_win;
-                other_win = oc;
+                current_win = panel_above((PANEL *)0);
                 top_panel(current_win);
                 cwm_window_refresh(current_win);
+                continue;
                 break;
             default:
                 break;
@@ -162,6 +185,27 @@ int main() {
             }
         }
         
+        if (mode == edit_mode) {
+            switch (key) {
+                case KEY_BACKSPACE:
+                    debug_print("Backspace\n");
+                    ((CWM_WINDOW_DATA *)panel_userptr(current_win))->window_buffer[((CWM_WINDOW_DATA *)panel_userptr(current_win))->window_buffer_end_pos - 1] = ' ';
+                    ((CWM_WINDOW_DATA *)panel_userptr(current_win))->window_buffer_end_pos--;
+                    break;
+                case KEY_END:
+                    debug_print("END\n");
+                    mode = no_mode;
+                    cwm_window_status(current_win, 0);
+                    break;
+                default:
+                    debug_print("Key: %c\n", key);
+                    ((CWM_WINDOW_DATA *)panel_userptr(current_win))->window_buffer[((CWM_WINDOW_DATA *)panel_userptr(current_win))->window_buffer_end_pos] = key;
+                    ((CWM_WINDOW_DATA *)panel_userptr(current_win))->window_buffer_end_pos++;
+                    break;
+            }
+            cwm_window_refresh(current_win);
+        }
+        
         debug_print("\n\n");
     }
 
@@ -180,6 +224,10 @@ PANEL *cwm_window_create(int height, int width, int y, int x) {
     obj->height = height;
     obj->width = width;
     obj->status = 0;
+    
+    for (int i = 0; i <= WINDOW_TEXT_BUFFER_SIZE; i++) obj->window_buffer[i] = ' '; // TODO: Do i need
+    obj->window_buffer_start_pos = 0;
+    obj->window_buffer_end_pos = 0;
 
     set_panel_userptr(pan, obj);
 
@@ -190,12 +238,17 @@ PANEL *cwm_window_create(int height, int width, int y, int x) {
 }
 
 void cwm_window_refresh(PANEL *panel) {
+    int pos = ((CWM_WINDOW_DATA *)panel_userptr(panel))->window_buffer_start_pos;
+
     wmove(panel_window(panel), 1, 1);
-    wprintw(panel_window(panel), "Test");
+
+    wprintw(panel_window(panel), ((CWM_WINDOW_DATA *)panel_userptr(panel))->window_buffer);
+
+    //debug_print("Current window buffer: %s\n", ((CWM_WINDOW_DATA *)panel_userptr(panel))->window_buffer); // Segfault ...
+
     cwm_window_status(panel, ((CWM_WINDOW_DATA *)panel_userptr(panel))->status);
     update_panels();
     doupdate();
-//    wrefresh(cwm_win->window);
 }
 
 void cwm_window_move(PANEL *panel, int y, int x) {
