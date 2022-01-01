4b91e308f4a3c82e2fad38452a775f17818a5517
Rillian Grant <rillian.grant@gmail.com>
2022-01-01 11:36:42 +0000

    Massive simplify and proper shortcuts

---
+++ lib/headers/keycodes.h
@@ -0,0 +1,26 @@
+/**
+ * @file keycodes.h
+ * @author Rillian Grant (rillian.grant@gmail.com)
+ * @brief Those weird keycode things that ncurses recieves. May change dependin on terminal emulator.
+ * @version 1
+ * @date 2021-12-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+// TODO Figure out the proper way to do this.
+
+#define KEY_TAB 9
+
+#define ctrl_letter(x) ((x) & 0x1f)
+
+#define KEY_CTRL_UP     566
+#define KEY_CTRL_DOWN   525
+#define KEY_CTRL_LEFT   545
+#define KEY_CTRL_RIGHT  560
+
+#define KEY_ALT_UP      564
+#define KEY_ALT_DOWN    523
+#define KEY_ALT_LEFT    543
+#define KEY_ALT_RIGHT   558
